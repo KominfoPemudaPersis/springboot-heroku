9c964291833ec7a32b41d1a226f9ed47a654c88d
hendisantika <hendisantika@yahoo.co.id>
2019-02-05 07:03:44 +0700

    #1 - Initial commit

---
+++ src/main/java/com/halaqohit/springbootheroku/SpringbootHerokuApplication.java
@@ -0,0 +1,14 @@
+package com.halaqohit.springbootheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHerokuApplication.class, args);
+    }
+
+}
+
